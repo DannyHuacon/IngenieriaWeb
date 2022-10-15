d0edcad756ada3c54c61f3be28a3f400508bf10c
Danny Benjamin Huacon Zapata <danny.huacon@udla.edu.ec>
2022-10-15 01:39:05 -0500

    Agregar archivos de proyecto.

---
+++ IngenieriaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IngenieriaWeb.Startup))]
+namespace IngenieriaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
